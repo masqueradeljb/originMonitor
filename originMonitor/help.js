9ea08f652e485f90693813e97e0b439d87610f73
skangel <skangellfc@gmail.com>
2015-08-05 16:43:40 -0400

    display

--- originMonitor/help.js
+++ originMonitor/help.js
@@ -2,12 +2,23 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('whitelist').addEventListener('click',clickHandler1);
+  document.getElementById('blacklist').addEventListener('click',clickHandler2);
 });
 
+document.getElementById("list")
+
 function clickHandler1(e) {
-  setTimeout(newTab, 10);
+ 	setTimeout(newTab, 10);
+}
+
+function clickHandler2(e) {
+	setTimeout(display, 10);
 }
 
 function newTab() {
   chrome.tabs.create({url: 'help.html'});
+}
+
+function display() {
+ document.getElementById("monitor").style.visibility="visible";
 }
